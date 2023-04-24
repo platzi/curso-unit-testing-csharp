a6e133b7e3b2efbb5c26a572e5c3b6eeaab48438
Miguel Teheran <miguelteheran@gmail.com>
2023-04-24 16:46:15 -0500

    adding tipos assert

--- StringManipulation.Tests/StringOperationsTest.cs
+++ StringManipulation.Tests/StringOperationsTest.cs
@@ -12,11 +12,42 @@ namespace StringManipulation.Tests
         [Fact]
         public void ConcatenateStrings()
         {
+            // Arrange
             var strOperations = new StringOperations();
 
+            // Act
             var result = strOperations.ConcatenateStrings("Hello", "Platzi");
 
+            // Assert
+            Assert.NotNull(result);
+            Assert.NotEmpty(result);
             Assert.Equal("Hello Platzi", result);
+        }
+
+        [Fact]
+        public void IsPalindrome_True()
+        {
+            // Arrange
+            var strOperations = new StringOperations();
+
+            // Act
+            var result = strOperations.IsPalindrome("ama");
+
+            // Assert
+            Assert.True(result);
+        }
+
+        [Fact]
+        public void IsPalindrome_False()
+        {
+            // Arrange
+            var strOperations = new StringOperations();
+
+            // Act
+            var result = strOperations.IsPalindrome("hello");
+
+            // Assert
+            Assert.False(result);
         }
     }
 }
