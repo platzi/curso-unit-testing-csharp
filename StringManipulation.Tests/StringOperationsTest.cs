66b4f98ec14e0b027b8b06af40d3920980f1f790
Miguel Teheran <miguelteheran@gmail.com>
2023-04-24 16:13:25 -0500

    adding base project and first test

---
+++ StringManipulation.Tests/StringOperationsTest.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace StringManipulation.Tests
+{
+    public class StringOperationsTest
+    {
+        [Fact]
+        public void ConcatenateStrings()
+        {
+            var strOperations = new StringOperations();
+
+            var result = strOperations.ConcatenateStrings("Hello", "Platzi");
+
+            Assert.Equal("Hello Platzi", result);
+        }
+    }
+}
